619796c339eb9af32d902252faee6b8c113c7a97
Flowing <bitoreieru@gmail.com>
2018-12-14 22:53:14 +0900

    support token memo

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const request = require('superagent');
 var argv = require('minimist')(process.argv.slice(2));
 
-if (!argv.webapi || !argv.appid) {
+if (!argv.webapi || !argv.appid || !argv.memo ) {
     console.log('usage: \n' +
                  '--webapi="YOUR STEAM WEB API TOKEN" \n' +
-                 '--appid=APPID of your game')
+                 '--appid="APPID of your game" \n' +
+				 '--memo="Additional memo for your token"' )
 }
 
 request
-    .post('https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key=' + argv.webapi + '&appid=' + argv.appid + "&memo=" + Date.now().toString())
+    //.post('https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key=' + argv.webapi + '&appid=' + argv.appid + "&memo=" + Date.now().toString())
+	.post('https://api.steampowered.com/IGameServersService/CreateAccount/v1/?key=' + argv.webapi + '&appid=' + argv.appid + "&memo=" + argv.memo)
     .set('accept', 'json')
     .end((err, res) => {
          console.log(res.body.response.login_token);
